cd85c0d2108fe6f748fa0c8a734563958820c875
Chris Bednarski <banzaimonkey@gmail.com>
2014-04-06 12:46:52 -0700

    Added list of tweets to homepage

--- src/annotweet.php
+++ src/annotweet.php
@@ -5,7 +5,14 @@ require_once(__DIR__ . '/../config.php');
 
 $app = new \Slim\Slim();
 
-Twitter::$cacheDir = __DIR__ . '/cache';
+$cache_dir = __DIR__ . '/../cache';
+
+$twig_loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig = new Twig_Environment($twig_loader, array(
+    // 'cache' => $cache_dir
+));
+
+Twitter::$cacheDir = $cache_dir;
 Twitter::$cacheExpire = 60;
 
 $twitter = new Twitter(
@@ -20,12 +27,17 @@ function json_response($data) {
     echo json_encode($data);
 }
 
-$app->get('/', function() use ($config, $twitter) {
-    // $results = $twitter->load(Twitter::ME, 100, array('screen_name' => $config['accountName']));
+function get_tweets() {
+    global $twitter, $config;
+    return $twitter->load(Twitter::ME, 100, array('screen_name' => $config['accountName']));
+}
+
+$app->get('/', function() use ($config, $twig, $twitter) {
+    $twig->display('index.twig', array('tweets' => get_tweets()));
 });
 
 $app->get('/tweets', function() use ($config, $twitter) {
-    $results = $twitter->load(Twitter::ME, 100, array('screen_name' => $config['accountName']));
+    $results = get_tweets();
     json_response($results);
 });
 
