ed7c6b60b0efa47cceaacb08a37b36195ab75493
Chris Bednarski <banzaimonkey@gmail.com>
2014-04-06 15:02:51 -0700

    Automatically update the cache when a new item is posted to twitter

--- src/annotweet.php
+++ src/annotweet.php
@@ -22,13 +22,38 @@ $twitter = new Twitter(
     $config['access_token_secret']
 );
 
-function json_response($data) {
+function get_twitter_cache($cache_dir)
+{
+    $files = scandir($cache_dir, SCANDIR_SORT_ASCENDING);
+    foreach($files as $file) {
+        if(substr($file, 0, 8) === 'twitter.') {
+            return $cache_dir . '/' . $file;
+        }
+    }
+    return null;
+}
+
+function prepend_cache($cache_dir, $item)
+{
+    $cache_file = get_twitter_cache($cache_dir);
+    if ($cache_file) {
+        $cache = json_decode(file_get_contents($cache_file));
+    } else {
+        $cache = array();
+    }
+    array_unshift($cache, $item);
+    file_put_contents($cache_file, json_encode($cache));
+}
+
+function json_response($data)
+{
     header('Content-Type: application/json', true);
     echo json_encode($data);
 }
 
-function get_tweets() {
-    global $twitter, $config;
+function get_tweets()
+{
+    global $twitter;
     return $twitter->load(Twitter::ME, 100);
 }
 
@@ -45,10 +70,17 @@ $app->get('/submit', function() use ($config, $twig) {
     $twig->display('form.twig');
 });
 
-$app->post('/submit', function() use ($app, $config, $twitter) {
+$app->post('/submit', function() use ($app, $config, $twitter, $twig, $cache_dir) {
     $tweet = $app->request->post('tweet');
     if (strlen($tweet) <= 140) {
-        $twitter->send($tweet);
+        try {
+            $response = $twitter->send($tweet);
+            if (property_exists($response, 'text') && $response->text === $tweet) {
+                prepend_cache($cache_dir, $response);
+            }
+        } catch(TwitterException $e) {
+            $twig->display('form.twig', array('error' => $e->getMessage()));
+        }
     }
     $app->redirect('/');
 });
