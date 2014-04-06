6731219053869f7a6f752442ad99d4b3828aae95
Chris Bednarski <banzaimonkey@gmail.com>
2014-04-06 10:33:03 -0700

    Added twitter initialization code to the main script

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
 
 require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . '/config.php');
 
 $app = new \Slim\Slim();
+$twitter = new Twitter(
+    $config['apiKey'],
+    $config['apiSecret'],
+    $config['accessToken'],
+    $config['accessTokenSecret']
+);
 
 $app->get('/', function() {
     echo 'hello';
 });
 
-$app->get('/twitter-feed', function() {
+$app->get('/update-twitter-feed', function() {
     echo 'twitter-feed';
 });
 
